90c6d25b408f5190b4b149ae4707df1718bc57af
pauline <pauline_rousseau@hotmail.co.uk>
2016-07-06 20:01:40 +0000

    longestNameAndMail

--- function.js
+++ function.js
@@ -13,4 +13,44 @@ function longestString(a,b){
    return strArray;
  }
 console.log(longestString("a", "ba"));
-    
+
+
+//array of objects
+
+var person1 = {
+    name : "pau",
+    mail :"clemclem"
+}
+var person2 = {
+    name : "mummy",
+    mail : "rousseaueli"
+}
+var objects=[person1, person2];
+
+objects.sort(function(person1, person2){
+              
+    if (person1.name.length>person2.name.length){
+       return 1;
+   }else if(person1.name.length<person2.name.length){
+       return -1;
+   }else{
+        return 0;
+       
+ }
+   })           
+ console.log(objects);
+ 
+ objects.sort(function(person1, person2) {
+      if (person1.mail>person2.mail){
+       return -1;
+   }else if(person1.mail<person2.mail){
+       return 1;
+   }else{
+        return 0;
+       }
+   })
+
+console.log(objects);
+ 
+ 
+   
